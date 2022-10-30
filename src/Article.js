85005d57946ef78856b355cfb467f6577cb679dc
Marta Pauk <martha_pauk0220@yahoo.com>
2022-10-30 13:42:27 +0200

    Add rel="noreferrer"

--- src/Article.js
+++ src/Article.js
@@ -1,20 +1,16 @@
 import React from "react";
 export default function Article({ article }) {
-  console.log(article.urlToImage);
   return (
-    <div className="Article">
-      <div className="border">
-        <a href={article.url} target="_blank" rel="noopener">
-          <h2> {article.title}</h2>
-        </a>
-
-        <p className="text-muted">{article.content}</p>
-        <img
-          src={article.urlToImage}
-          alt={article.title}
-          className="img-fluid"
-        ></img>
-      </div>
+    <div className="Article border m-2">
+      <a href={article.url} target="_blank" rel="noreferrer">
+        <h2> {article.title}</h2>
+      </a>
+      <p className="text-muted">{article.content}</p>
+      <img
+        src={article.urlToImage}
+        alt={article.title}
+        className="img-fluid"
+      ></img>
     </div>
   );
 }
